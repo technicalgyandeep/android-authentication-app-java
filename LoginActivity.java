12891f544e8841a4d5a931e2677099e7f8003422
Techy Hunt <62538708+technicalgyandeep@users.noreply.github.com>
2020-08-26 20:24:15 +0530

    initial commit

---
+++ LoginActivity.java
@@ -0,0 +1,266 @@
+package com.my.newproject19;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.annotation.*;
+import android.app.*;
+import android.os.*;
+import android.view.*;
+import android.view.View.*;
+import android.widget.*;
+import android.content.*;
+import android.graphics.*;
+import android.media.*;
+import android.net.*;
+import android.text.*;
+import android.util.*;
+import android.webkit.*;
+import android.animation.*;
+import android.view.animation.*;
+import java.util.*;
+import java.text.*;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.EditText;
+import android.widget.Button;
+import android.content.Intent;
+import android.net.Uri;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import android.view.View;
+import android.graphics.Typeface;
+
+public class LoginActivity extends AppCompatActivity {
+	
+	
+	private LinearLayout linear1;
+	private TextView textview1;
+	private TextView textview2;
+	private EditText edittext1;
+	private EditText edittext2;
+	private TextView textview3;
+	private Button button1;
+	
+	private Intent u = new Intent();
+	private FirebaseAuth auth;
+	private OnCompleteListener<Void> auth_updateEmailListener;
+	private OnCompleteListener<Void> auth_updatePasswordListener;
+	private OnCompleteListener<Void> auth_emailVerificationSentListener;
+	private OnCompleteListener<Void> auth_deleteUserListener;
+	private OnCompleteListener<Void> auth_updateProfileListener;
+	private OnCompleteListener<AuthResult> auth_phoneAuthListener;
+	private OnCompleteListener<AuthResult> auth_googleSignInListener;
+	private OnCompleteListener<AuthResult> _auth_create_user_listener;
+	private OnCompleteListener<AuthResult> _auth_sign_in_listener;
+	private OnCompleteListener<Void> _auth_reset_password_listener;
+	@Override
+	protected void onCreate(Bundle _savedInstanceState) {
+		super.onCreate(_savedInstanceState);
+		setContentView(R.layout.login);
+		com.google.firebase.FirebaseApp.initializeApp(this);
+		initialize(_savedInstanceState);
+		initializeLogic();
+	}
+	
+	private void initialize(Bundle _savedInstanceState) {
+		
+		linear1 = (LinearLayout) findViewById(R.id.linear1);
+		textview1 = (TextView) findViewById(R.id.textview1);
+		textview2 = (TextView) findViewById(R.id.textview2);
+		edittext1 = (EditText) findViewById(R.id.edittext1);
+		edittext2 = (EditText) findViewById(R.id.edittext2);
+		textview3 = (TextView) findViewById(R.id.textview3);
+		button1 = (Button) findViewById(R.id.button1);
+		auth = FirebaseAuth.getInstance();
+		
+		textview3.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View _view) {
+				u.setClass(getApplicationContext(), ForgetActivity.class);
+				startActivity(u);
+			}
+		});
+		
+		button1.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View _view) {
+				if (edittext1.getText().toString().equals("") && edittext2.getText().toString().equals("")) {
+					SketchwareUtil.showMessage(getApplicationContext(), "Please fill the Credentials ");
+				}
+				else {
+					auth.signInWithEmailAndPassword(edittext1.getText().toString(), edittext2.getText().toString()).addOnCompleteListener(LoginActivity.this, _auth_sign_in_listener);
+				}
+			}
+		});
+		
+		auth_updateEmailListener = new OnCompleteListener<Void>() {
+			@Override
+			public void onComplete(Task<Void> _param1) {
+				final boolean _success = _param1.isSuccessful();
+				final String _errorMessage = _param1.getException() != null ? _param1.getException().getMessage() : "";
+				
+			}
+		};
+		
+		auth_updatePasswordListener = new OnCompleteListener<Void>() {
+			@Override
+			public void onComplete(Task<Void> _param1) {
+				final boolean _success = _param1.isSuccessful();
+				final String _errorMessage = _param1.getException() != null ? _param1.getException().getMessage() : "";
+				
+			}
+		};
+		
+		auth_emailVerificationSentListener = new OnCompleteListener<Void>() {
+			@Override
+			public void onComplete(Task<Void> _param1) {
+				final boolean _success = _param1.isSuccessful();
+				final String _errorMessage = _param1.getException() != null ? _param1.getException().getMessage() : "";
+				
+			}
+		};
+		
+		auth_deleteUserListener = new OnCompleteListener<Void>() {
+			@Override
+			public void onComplete(Task<Void> _param1) {
+				final boolean _success = _param1.isSuccessful();
+				final String _errorMessage = _param1.getException() != null ? _param1.getException().getMessage() : "";
+				
+			}
+		};
+		
+		auth_phoneAuthListener = new OnCompleteListener<AuthResult>() {
+			@Override
+			public void onComplete(Task<AuthResult> task){
+				final boolean _success = task.isSuccessful();
+				final String _errorMessage = task.getException() != null ? task.getException().getMessage() : "";
+				
+			}
+		};
+		
+		auth_updateProfileListener = new OnCompleteListener<Void>() {
+			@Override
+			public void onComplete(Task<Void> _param1) {
+				final boolean _success = _param1.isSuccessful();
+				final String _errorMessage = _param1.getException() != null ? _param1.getException().getMessage() : "";
+				
+			}
+		};
+		
+		auth_googleSignInListener = new OnCompleteListener<AuthResult>() {
+			@Override
+			public void onComplete(Task<AuthResult> task){
+				final boolean _success = task.isSuccessful();
+				final String _errorMessage = task.getException() != null ? task.getException().getMessage() : "";
+				
+			}
+		};
+		
+		_auth_create_user_listener = new OnCompleteListener<AuthResult>() {
+			@Override
+			public void onComplete(Task<AuthResult> _param1) {
+				final boolean _success = _param1.isSuccessful();
+				final String _errorMessage = _param1.getException() != null ? _param1.getException().getMessage() : "";
+				
+			}
+		};
+		
+		_auth_sign_in_listener = new OnCompleteListener<AuthResult>() {
+			@Override
+			public void onComplete(Task<AuthResult> _param1) {
+				final boolean _success = _param1.isSuccessful();
+				final String _errorMessage = _param1.getException() != null ? _param1.getException().getMessage() : "";
+				if (_success) {
+					u.setClass(getApplicationContext(), HomeActivity.class);
+					startActivity(u);
+					finish();
+				}
+				else {
+					SketchwareUtil.showMessage(getApplicationContext(), _errorMessage);
+				}
+			}
+		};
+		
+		_auth_reset_password_listener = new OnCompleteListener<Void>() {
+			@Override
+			public void onComplete(Task<Void> _param1) {
+				final boolean _success = _param1.isSuccessful();
+				
+			}
+		};
+	}
+	private void initializeLogic() {
+		textview1.setTypeface(Typeface.createFromAsset(getAssets(),"fonts/poppins.ttf"), 0);
+		textview2.setTypeface(Typeface.createFromAsset(getAssets(),"fonts/poppins.ttf"), 0);
+		edittext1.setTypeface(Typeface.createFromAsset(getAssets(),"fonts/poppins.ttf"), 0);
+		edittext2.setTypeface(Typeface.createFromAsset(getAssets(),"fonts/poppins.ttf"), 0);
+		textview3.setTypeface(Typeface.createFromAsset(getAssets(),"fonts/poppins.ttf"), 0);
+		button1.setTypeface(Typeface.createFromAsset(getAssets(),"fonts/poppins.ttf"), 0);
+	}
+	
+	@Override
+	protected void onActivityResult(int _requestCode, int _resultCode, Intent _data) {
+		
+		super.onActivityResult(_requestCode, _resultCode, _data);
+		
+		switch (_requestCode) {
+			
+			default:
+			break;
+		}
+	}
+	
+	@Deprecated
+	public void showMessage(String _s) {
+		Toast.makeText(getApplicationContext(), _s, Toast.LENGTH_SHORT).show();
+	}
+	
+	@Deprecated
+	public int getLocationX(View _v) {
+		int _location[] = new int[2];
+		_v.getLocationInWindow(_location);
+		return _location[0];
+	}
+	
+	@Deprecated
+	public int getLocationY(View _v) {
+		int _location[] = new int[2];
+		_v.getLocationInWindow(_location);
+		return _location[1];
+	}
+	
+	@Deprecated
+	public int getRandom(int _min, int _max) {
+		Random random = new Random();
+		return random.nextInt(_max - _min + 1) + _min;
+	}
+	
+	@Deprecated
+	public ArrayList<Double> getCheckedItemPositionsToArray(ListView _list) {
+		ArrayList<Double> _result = new ArrayList<Double>();
+		SparseBooleanArray _arr = _list.getCheckedItemPositions();
+		for (int _iIdx = 0; _iIdx < _arr.size(); _iIdx++) {
+			if (_arr.valueAt(_iIdx))
+			_result.add((double)_arr.keyAt(_iIdx));
+		}
+		return _result;
+	}
+	
+	@Deprecated
+	public float getDip(int _input){
+		return TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, _input, getResources().getDisplayMetrics());
+	}
+	
+	@Deprecated
+	public int getDisplayWidthPixels(){
+		return getResources().getDisplayMetrics().widthPixels;
+	}
+	
+	@Deprecated
+	public int getDisplayHeightPixels(){
+		return getResources().getDisplayMetrics().heightPixels;
+	}
+	
+}
